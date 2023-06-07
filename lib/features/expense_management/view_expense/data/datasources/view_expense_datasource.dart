293a2f890b13f906a9d2c02773251aaa81982fff
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2023-06-07 17:16:33 +0600

    cleared view_expense

--- lib/features/expense_management/view_expense/data/datasources/view_expense_datasource.dart
+++
@@ -1,5 +0,0 @@
-import '../../domain/entities/view_expense_entity.dart';
-
-abstract class ViewExpenseDataSource {
-  Future<List<ViewExpenseEntity>> getExpenses();
-}
--- lib/features/expense_management/view_expense/domain/repositories/view_expense_repository.dart
+++
@@ -1,5 +0,0 @@
-import "../entities/view_expense_entity.dart";
-
-abstract class ViewExpenseRepository {
-  Future<List<ViewExpenseEntity>> getExpenses();
-}
