919a9c053c0b8f0266290a355703f12999f4009e
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2023-06-07 13:53:43 +0600

    .

--- lib/features/user_management/data/datasources/user_datasource_impl.dart
+++ lib/features/user_management/data/datasources/user_datasource_impl.dart
@@ -1,7 +1,5 @@
 import 'package:nothing/features/user_management/data/datasources/user_datasource.dart';
 import 'package:nothing/features/user_management/data/models/user_model.dart';
-import 'package:nothing/features/user_management/domain/entities/user_entity.dart';
-import 'package:nothing/features/user_management/domain/repositories/user_repository.dart';
 import 'package:nothing/features/user_management/data/datasources/user_database.dart';
 
 class UserDataSourceImpl implements UserDataSource {
