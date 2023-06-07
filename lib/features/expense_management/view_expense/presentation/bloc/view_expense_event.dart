1e4c485d48e47b7df16051a1dc5755d76efb8862
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2023-06-07 17:29:28 +0600

    created expense_mangement

--- lib/features/expense_management/view_expense/presentation/bloc/view_expense_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'view_expense_bloc.dart';
-
-abstract class ViewExpenseEvent extends Equatable {
-  const ViewExpenseEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- lib/features/expense_management/view_expense/presentation/bloc/view_expense_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'view_expense_bloc.dart';
-
-abstract class ViewExpenseState extends Equatable {
-  const ViewExpenseState();  
-
-  @override
-  List<Object> get props => [];
-}
-class ViewExpenseInitial extends ViewExpenseState {}
