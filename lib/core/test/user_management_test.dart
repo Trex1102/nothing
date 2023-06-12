ec873d06194539d8d7ac2874d90baac692df25e0
Isaba Ishrak <isabaishrak@iut-dhaka.edu>
2023-06-13 03:31:30 +0600

    Update user_management_test.dart

--- lib/core/test/user_management_test.dart
+++ lib/core/test/user_management_test.dart
@@ -74,3 +74,152 @@ void main() async {
     );
   });
 }
+
+// class MyApp extends StatelessWidget {
+//   final UserRepository userRepository;
+
+//   MyApp({required this.userRepository});
+
+//   @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//     title: 'demo',
+//     home: MultiBlocProvider(
+//       providers: [
+//         BlocProvider<LoginBloc>(
+//           create: (_) => LoginBloc(loginUserUseCase: LoginUserUseCase(userRepository)),
+//         ),
+//         BlocProvider<RegisterBloc>(
+//           create: (_) => RegisterBloc(registerUserUseCase: RegisterUserUseCase(userRepository)),
+//         ),
+//       ],
+//       child: HomePage(),
+//     ),
+//   );
+// }
+
+// }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final registerBloc = BlocProvider.of<RegisterBloc>(context);
+//     final loginBloc = BlocProvider.of<LoginBloc>(context);
+
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             BlocConsumer<RegisterBloc, RegisterState>(
+//               listener: (context, state) {
+//                 if (state is RegisterSuccess) {
+//                   showDialog(
+//                       context: context,
+//                       builder: (_) => AlertDialog(
+//                             title: Text('Reg success'),
+//                             content: Text('suc'),
+//                             actions: [
+//                               TextButton(
+//                                 onPressed: () {
+//                                   Navigator.pop(context);
+//                                 },
+//                                 child: Text('ok'),
+//                               ),
+//                             ],
+//                           ));
+//                 } else if (state is RegisterFailure) {
+//                   showDialog(
+//                       context: context,
+//                       builder: (_) => AlertDialog(
+//                             title: Text('Reg failed'),
+//                             content: Text('ff'),
+//                             actions: [
+//                               TextButton(
+//                                 onPressed: () {
+//                                   Navigator.pop(context);
+//                                 },
+//                                 child: Text('ok'),
+//                               ),
+//                             ],
+//                           ));
+//                 }
+//               },
+//               builder: (context, state) {
+//                 return ElevatedButton(
+//                   onPressed: () {
+//                     registerBloc.add(
+//                       RegisterButtonPressed(
+//                         user: UserEntity(
+//                           id: '1',
+//                           username: 'ibasa',
+//                           email: 'ii@example.com',
+//                           password: 'pass123',
+//                         ),
+//                       ),
+//                     );
+//                   },
+//                   child: const Text('Register'),
+//                 );
+//               },
+//             ),
+//             BlocConsumer<LoginBloc, LoginState>(
+//           listener: (context, state) {
+//             if (state is LoginSuccess) {
+//               showDialog(
+//                   context: context,
+//                   builder: (_) => AlertDialog(
+//                         title: Text('login success'),
+//                         content: Text('suc'),
+//                         actions: [
+//                           TextButton(
+//                             onPressed: () {
+//                               Navigator.pop(context);
+//                             },
+//                             child: Text('ok'),
+//                           ),
+//                         ],
+//                       ));
+//             } else if (state is LoginFailure) {
+//               showDialog(
+//                   context: context,
+//                   builder: (_) => AlertDialog(
+//                         title: Text('login failed'),
+//                         content: Text('ff'),
+//                         actions: [
+//                           TextButton(
+//                             onPressed: () {
+//                               Navigator.pop(context);
+//                             },
+//                             child: Text('ok'),
+//                           ),
+//                         ],
+//                       ));
+//             }
+//           },
+//           builder: (context, state) {
+//             return ElevatedButton(
+//               onPressed: () {
+//                 loginBloc.add(
+//                   LoginButtonPressed(
+//                     user: UserEntity(
+//                       id: '1',
+//                       username: 'ibasa',
+//                       email: 'ii@example.com',
+//                       password: 'pass123',
+//                     ),
+//                   ),
+//                 );
+//               },
+//               child: const Text('Login'),
+//             );
+//           },
+//         ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
