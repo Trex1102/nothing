408ab54aa9af18b091ea714ed256809de60629f6
FarzanaChowdhury <sumaiya.farzana.eleen@gmail.com>
2023-06-13 06:03:00 +0600

    Corrected Expense Screen

--- lib/features/homepage/presentation/pages/expense_screen.dart
+++ lib/features/homepage/presentation/pages/expense_screen.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:nothing/features/expense_management/presentation/get_expenses_by_user_presenter/widgets/expense_list.dart';
-import 'package:nothing/features/expense_management/presentation/get_expenses_by_user_presenter/widgets/homepage_upper_navbar.dart';
 
 import '../../../common/presentation/widgets/bottom_navbar.dart';
 import '../../../expense_management/presentation/get_expenses_by_user_presenter/widgets/expense_tab.dart';
 import '../../../income_management/presentation/get_income_by_id_presentor/widgets/income_tab.dart';
 import '../../../statistics/presentation/pages/statistics_screen.dart';
-
-// class HomepageExpenseScreen extends StatelessWidget {
-//   const HomepageExpenseScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: Container(
-//         alignment: Alignment.center,
-//         child: Column(
-//           children: [
-//             HomepageUpperNavbar(),
-//             //DailyExpense(),
-//             //ExpenseTile(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import '../widgets/upper_navbar.dart';
 
 class HomepageExpenseScreen extends StatelessWidget {
   @override
@@ -33,25 +12,16 @@ class HomepageExpenseScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2, // Number of tabs
       child: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              // IconButton(
-              //   onPressed: () {},
-              //   icon: Icon(Icons.arrow_back),
-              // ),
-              Icon(
-                Icons.account_balance,
-                color: Color.fromARGB(255, 217, 191, 0),
+        //appBar: setAppBar(),
+        body: Column(
+          children: [
+            setAppBar(),
+            Expanded(
+              child: TabBarView(
+                children: [ExpenseTab(), IncomeTab()],
               ),
-              Text('FinTracker')
-            ],
-          ),
-          // backgroundColor: Color.fromARGB(255, 217, 217, 217),
-          backgroundColor: Colors.white,
-        ),
-        body: TabBarView(
-          children: [ExpenseTab(), IncomeTab()],
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
